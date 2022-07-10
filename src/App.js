9030a3da697417b2b36ffc2e8950040ebf7c304c
System Administrator <root@503800462.lan>
2022-07-09 21:39:28 -0700

    edition one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './index.css';
+import StandardImageList from './imageList.js';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  document.body.style.backgroundColor = "#F0C0C9";
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("TitleLogo").style.height = "25vh";
+    document.getElementById("TitleLogo").style.top = "5vh";
+  //  document.getElementById("TitleLogo").style.backgroundColor = "white";
+  //  document.getElementById("TitleLogo").style.animation = "";
+
+		document.getElementById("nav").style.top = "2vh";
+
+
+
+  } else {
+    document.getElementById("TitleLogo").style.height = "35vh";
+    document.getElementById("TitleLogo").style.top = "4vh";
+    //document.getElementById("TitleLogo").style.backgroundColor = "";
+    //document.getElementById("TitleLogo").style.animation = "pulsefade 4s ease-in-out infinite alternate";
+
+
+		document.getElementById("nav").style.top = "25vh";
+
+
+  }
+	if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 400) {
+
+	} else {
+		document.body.style.backgroundColor = "#d8e8ef";
+	}
+
+
+
+}
+let tagapp = "all";
+
+function Home(){
+	return(
+		<span>
+	<Logo/>
+	<Footer/>
+	<StandardImageList tag="all" gridColumns="2"/>
+	</span>
+);
+}
+
+function Rad(props){
+	return(
+		<span>
+	<Logo/>
+	<Footer/>
+	<StandardImageList tag={props.tag} gridColumns="2"/>
+	</span>
+);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+	return (
+		<span>
+
+		<Routes>
+       <Route path='/cars' element={<Rad tag="CAR"  /> } />
+       <Route  path='/' element={<Home /> } />
+			 <Route path='/usa' element={<Rad tag="USA"  />} />
+			 <Route path='/ppl' element={<Rad tag="ppl"  />} />
+			 <Route path='/out' element={<Rad tag="out" /> } />
+			 <Route path='/commod' element={<Rad tag="prop0623" /> } />
+       <Route path='/rad' element={<Rad tag="RAD" /> } />
+       <Route path='/prop0623' element={<Rad tag="prop0623" /> } />
+       <Route path='/midjourneyPrompts' element={<Rad tag="midjourney"/> } />
+
+    </Routes>
+</span>
+
+	);
+
+
+}
+
+
+
+function Logo(){
+
+	return(
+		<span>
+		<a href="/" >
+	<img  id="TitleLogo" alt="Schidt.co Logo" src="https://i.imgur.com/C2Hbz1u.png"/>
+		</a>
+
+		<div id="nav" class="nav">
+		<a href="#/cars" ><b class="b bionic">thir</b>di </a>
+		<a href="#/usa"> <b class="b bionic">thir</b>di </a>
+    <a href="#/cars" ><b class="b bionic">thir</b>di </a>
+    <a href="#/cars" ><b class="b bionic">thir</b>di </a>
+    <a href="#/cars" ><b class="b bionic">thir</b>di </a>
+
+		</div>
+
+		</span>
+);
+}
+function unusued(){
+  //<a href="https://instagram.com/schidt.co/">
+//  <img  id="igLogo" alt="Schidt.co IG Logo" src="https://i.imgur.com/inu4zIh.png" />
+//  </a>
+}
+
+
+function Footer(){
+	return(
+<div id="footer">
+<span style={{backgroundColor: "#d8e8ef"}}>
+natepsacc@gmail.com, @schidtery,
+<br/>Schidt.co is a Web3 site, welcome to the new world.
+
+</span>
+</div>
+);
 }
 
 export default App;
---
+++ src/imageList.js
@@ -0,0 +1,205 @@
+import * as React from 'react';
+import {useState} from 'react';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Box from '@mui/material/Box';
+import { typography } from '@mui/system';
+import imageListItemClasses from "@material-ui/core/ImageListItem";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+
+
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      bigMobile: 300,
+      tablet: 650,
+      desktop: 1000
+    }
+  },
+  palette: {
+    background: {
+      paper: 'rgba(0,0,0,0.2)'
+    }}
+});
+
+export default function StandardImageList(props) {
+
+
+  let sort = itemData.filter((element) => {
+      if (element.tag === props.tag) {
+          return element;
+      }
+      if (element.tag2 === props.tag) {
+          return element;
+      }
+      if (element.text === 'text') {
+  //      document.getElementsByClassName('imgsvg').append;
+      }
+      if (element.tag3 === 'noshow') {
+          return !element;
+      }
+
+      else{
+
+      }
+      if (props.tag === "all"){
+        return itemData
+      }
+  });
+
+var gridNum = props.gridColumns;
+console.log(gridNum);
+const imageClick = (props) => {
+
+  console.log('Click!!!!');
+  window.open(props, '_self');
+
+}
+  const [clicked, setClicked] = useState(false)
+  const toggleClicked = () => setClicked((prev) => !prev)
+      return (
+
+      <ThemeProvider theme={theme}>
+
+   <Box
+   onClick={toggleClicked}
+     sx={{
+       height: {
+         mobile: clicked ? "35vh" : "600px",
+         bigMobile: clicked ? "35vh" : "85vh",
+         tablet: "100vh",
+         desktop: "100vh"
+       },
+       display: "grid",
+       gridTemplateColumns:{
+         mobile: clicked ? 'repeat(2, 50%)' : 'repeat(1, 100%)',
+         bigMobile: clicked ? 'repeat(2, 50%)' : 'repeat(1, 100%)',
+         desktop: clicked ? 'repeat(1, 100%)' : 'repeat(2, 50%)',
+       },
+       [`& .${imageListItemClasses.root}`]: {
+         display: "flex",
+         flexDirection: "column"
+       }
+     }}
+   >
+
+     {sort.map((item) => (
+
+
+       <ImageListItem
+id='imgimg' key={item.img} id="height" sx={{m:0}}>
+
+       <LazyLoadImage
+
+          threshhold='1000'
+           width="100%"
+           height="inherit"
+           onClick={() => imageClick(item.mSrc)}
+
+             effect="blur"
+            alt={item.title}
+            src={item.img} // use normal <img> attributes as props
+             placeholderSrc="https://i.imgur.com/4GdCjPE.gif"
+             />
+
+
+
+              <ImageListItemBar
+              sx={{
+                overflowWrap:"break-word",
+
+                bgcolor:'background.paper',
+                maxWidth: "100%",
+                height: 'fit-content',
+                width:'fit-content',
+                textAlign: 'left',
+                fontFamily: 'Courier New',
+                fontSize: {
+                  mobile: "1em",
+                bigMobile: "1em",
+                tablet: "12pt",
+                desktop: "12pt"}
+                  }}
+            title={item.date}
+            subtitle={item.title}
+
+          />
+
+       </ImageListItem>
+
+     ))}
+
+
+   </Box>
+ </ThemeProvider>
+
+      );
+}
+
+
+
+
+const itemData = [
+  {
+      img: 'https://i.imgur.com/kQVedhu.jpg',
+      title: 'thirdi!',
+      date: 'thirdi',
+      tag: 'USA',
+      tag2: 'ppl',
+      mSrc: 'javascript:;',
+  },
+  {
+     img: 'https://i.imgur.com/EQPZ1ep.jpg',
+     title: 'thirdi',
+     tag: 'USA',
+     tag2: 'ppl',
+     date: 'thirdi',
+     mSrc: 'javascript:;',
+ },
+ {
+     img: 'https://i.imgur.com/TbGLFYZ.jpg',
+     title: 'thirdi!',
+     date: 'thirdi',
+     tag: 'USA',
+     tag2: 'ppl',
+     mSrc: 'javascript:;',
+ },
+ {
+    img: 'https://i.imgur.com/YxkyjUa.jpg',
+    title: 'thirdi',
+    tag: 'USA',
+    tag2: 'ppl',
+    date: 'thirdi',
+    mSrc: 'javascript:;',
+},
+{
+    img: 'https://i.imgur.com/BqZGmpq.jpg',
+    title: 'thirdi!',
+    date: 'thirdi',
+    tag: 'USA',
+    tag2: 'ppl',
+    mSrc: 'javascript:;',
+},
+ {
+     img: 'https://i.imgur.com/7qo6dWx.jpg',
+     title: 'thirdi!',
+     date: 'thirdi',
+     tag: 'USA',
+     tag2: 'ppl',
+     mSrc: 'javascript:;',
+ },
+ {
+     img: 'https://i.imgur.com/99hpZyC.jpg',
+     title: 'thirdi!',
+     date: 'thirdi',
+     tag: 'USA',
+     tag2: 'ppl',
+     mSrc: 'javascript:;',
+ },
+];
